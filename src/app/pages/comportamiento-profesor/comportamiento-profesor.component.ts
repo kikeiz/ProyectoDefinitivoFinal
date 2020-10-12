27cc017fabfee6b929c8c7810dc0451a37039873
Kike Izquierdo <kikeizp96@gmail.com>
2020-10-12 11:20:17 +0200

    actualizacion

--- src/app/pages/comportamiento-profesor/comportamiento-profesor.component.ts
+++ src/app/pages/comportamiento-profesor/comportamiento-profesor.component.ts
@@ -6,10 +6,85 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comportamiento-profesor.component.css']
 })
 export class ComportamientoProfesorComponent implements OnInit {
-
-  constructor() { }
+  public puntualidad: boolean
+  public deberes: boolean
+  public atencion:boolean
+  public comportamiento:boolean
+  public rojo:boolean
+  public verde:boolean
+  public tablaResumen:boolean
+  public nombre:string
+  public mostrarF4:boolean
+  constructor(){
+    this.puntualidad = false
+    this.deberes = false
+    this.atencion = false
+    this.comportamiento = false
+    this.rojo = false
+    this.verde = false
+    this.tablaResumen = false
+    this.mostrarF4 = false
+   }
 
   ngOnInit(): void {
   }
 
+  resaltar(valor:number){
+    if(valor == 0){
+      this.puntualidad = true;
+      this.atencion = false;
+      this.comportamiento = false;
+      this.deberes = false
+    }else if(valor == 1){
+      this.atencion = true;
+      this.comportamiento = false;
+      this.deberes = false;
+      this.puntualidad = false;
+    }else if(valor ==2){
+      this.deberes = true;
+      this.atencion = false;
+      this.comportamiento = false;
+      this.puntualidad = false;
+    }else{
+      this.comportamiento = true;
+      this.deberes = false;
+      this.atencion = false;
+      this.puntualidad = false;
+    }
+  }
+
+  color(color:number){
+    if(color == 0){
+      this.rojo = true
+      this.verde = false
+    }else{
+      this.verde = true
+      this.rojo = false
+    }
+  }
+
+  actualizar(){
+    this.rojo = false
+    this.verde = false
+    this.comportamiento = false
+    this.atencion = false
+    this.deberes = false
+    this.puntualidad = false
+  }
+
+  mostrar(tipo:string){
+    this.tablaResumen = true
+    this.nombre = tipo
+  }
+  cambiar(){
+    this.mostrarF4 = true
+   }
+ 
+   modificar(){
+     this.mostrarF4 = false
+   }
+
+   terminar(){
+     this.tablaResumen = false
+   }
 }
--- src/app/pages/notas-profesor/notas-profesor.component.ts
+++ src/app/pages/notas-profesor/notas-profesor.component.ts
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notas-profesor.component.css']
 })
 export class NotasProfesorComponent implements OnInit {
-
-  constructor() { }
+  public mostrarF1: boolean
+  public mostrarF2: boolean
+  public mostrarF3: boolean
+  public mostrarF4: boolean
+  constructor() { 
+    this.mostrarF1 = false
+    this.mostrarF2 = false
+    this.mostrarF3 = false
+    this.mostrarF4 = false
+  }
 
   ngOnInit(): void {
   }
+  anadirNota(){
+    this.mostrarF1 = true
+  }
 
+  insertarNotas(){
+    this.mostrarF2 = true
+  }
+
+  actualizar(){
+    this.mostrarF1 = false
+    this.mostrarF2 = false
+  }
+
+  editar(){
+    this.mostrarF3 = true
+  }
+
+  cerrar(){
+    this.mostrarF1 = false
+    this.mostrarF2 = false
+    this.mostrarF3 = false
+  }
+
+  cambiar(){
+   this.mostrarF4 = true
+  }
+
+  modificar(){
+    this.mostrarF4 = false
+  }
 }
