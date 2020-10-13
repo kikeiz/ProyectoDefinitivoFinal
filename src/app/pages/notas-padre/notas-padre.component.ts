92cb7894f99c1af64d1b213b13e6d990be0bd8ee
Kike Izquierdo <kikeizp96@gmail.com>
2020-10-13 15:10:39 +0200

    notasPadre

--- src/app/pages/notas-padre/notas-padre.component.ts
+++ src/app/pages/notas-padre/notas-padre.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TickOptions } from 'chart.js';
+import { Nota } from 'src/app/models/nota';
+import {Tipo} from 'src/app/models/nota'
 
 @Component({
   selector: 'app-notas-padre',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notas-padre.component.css']
 })
 export class NotasPadreComponent implements OnInit {
-
-  constructor() { }
+  public options: boolean
+  public notas: Nota []
+  constructor() { 
+    this.options = false
+    this.notas = [new Nota(7.5, "1998-07-08", Tipo.examen, "biologia" ), 
+                  new Nota(4.5, "2008-07-08", Tipo.trabajo, "Matematicas" )]
+  }
 
   ngOnInit(): void {
   }
 
+  opciones(){
+    if(this.options==true){
+      this.options = false
+    }else{
+      this.options = true
+    }
+  }
+
+  filter(){
+    this.options = false
+  }
 }
