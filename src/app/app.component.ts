72ee08992f7c75f312982908ecc17694be5e475f
Kike Izquierdo <kikeizp96@gmail.com>
2020-10-14 12:06:13 +0200

    ultimoscambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { LoginService } from './shared/login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'ProyectoFinal';
+
+  constructor(public service:LoginService){
+    
+  }
 }
--- src/app/pages/login-register/login-register.component.ts
+++ src/app/pages/login-register/login-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/shared/login.service';
 
 @Component({
   selector: 'app-login-register',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-register.component.css']
 })
 export class LoginRegisterComponent implements OnInit {
-
-  constructor() { }
+  public profesor:boolean
+  constructor(public service:LoginService) {
+    this.profesor = true
+   }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    this.service.entrar()
+  }
+
+  registrarse(){
+    this.service.entrar()
+ 
+  }
+
+  cambiar(data:string){
+    if(data == 'padres'){
+      this.profesor = false  
+    }else{
+      this.profesor = true
+    }
+    this.service.navbar(data)
   }
 
 }
